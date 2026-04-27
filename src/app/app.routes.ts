46a729358aac73aa57a4518966b9deb22f26e2ec
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-04-27 08:30:00 +0200

    Mise en place de httpClient pour la création d'utilisateur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login'
+import { Signup } from './features/signup/signup';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'auth/login', component: LoginComponent },
+  { path: 'signup', component: Signup },
 ];
--- src/core/services/auth.service.ts
+++ src/core/services/auth.service.ts
@@ -1,14 +1,47 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+class User {
+  id: number;
+  email: string;
+  name: string;
+
+  constructor(id: number, email: string, name: string) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export default class AuthService {
+  private http = inject(HttpClient);
   private isAuthenticated: boolean = false;
-  private apiUrl: string = 'https://api.example.com/auth';
+  private apiDevUrl: string = 'http://localhost:8080';
+  private createRoute : string = '/api/users/create';
+  constructor(){
+  }
 
-  constructor(private http: HttpClient) {
+  signup(email: string, password: string, name: string) : boolean {
+    if (email && password && name) {
+      const url = this.apiDevUrl + this.createRoute;
+      const body = { email, password, name };
+
+      this.http.post<User>(url, body).subscribe({
+        next: (user) => {
+          console.log('Created user', user);
+          this.isAuthenticated = true;
+          return true;
+        },
+        error: (err) => {
+          console.error('Erreur création user', err);
+          return false;
+        }
+      });
+    }
+    return false;
   }
 
   login(email: string, password: string): boolean {
