15a6c09ca76fc7277d9fd200946ef0523d4d37cf
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-04-30 08:00:00 +0200

    feat: Add 404 not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login'
 import { Signup } from './features/signup/signup';
+import { NotFound } from './features/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
   { path: 'auth/login', component: LoginComponent },
   { path: 'signup', component: Signup },
+  { path: '**',pathMatch: 'full', component: NotFound }
 ];
